0c37186fbf36ec7e53e3e556ee4f40669f0d77fc
Sofia Rodrigues <sofia@algebraic.dev>
2025-02-23 22:04:57 -0300

    chore: update copyright

--- src/lib/parser.ml
+++ src/lib/parser.ml
@@ -1,5 +1,5 @@
 (*
-Copyright 2024 Lemonadic. All rights reserved.
+Copyright 2025 Lemonadic. All rights reserved.
 Licensed under the Apache License, Version 2.0 as described in the file LICENSE.
 
 Authors: Sofia Rodrigues
